22e11362e6f6620366273cec6e13a900cc8a75ec
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-08-19 03:20:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('case-plus').addEventListener('click', function () {
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value ;
+    caseInput.value = parseInt(caseNumber + 1);
+    
+})
