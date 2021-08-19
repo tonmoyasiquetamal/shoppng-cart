dbaa3a8cf910c070d60cc7470bc163762e02961b
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-08-19 15:39:46 +0600

    rehabation of code

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+/* for case plus & case number addition */
 document.getElementById('case-plus').addEventListener('click', function () {
     const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value ;
-    caseInput.value = parseInt(caseNumber + 1);
-    
+    caseInput.value = parseInt(caseNumber) +1;
+});
+/* for case minus & case number subtraction */
+document.getElementById('case-minus').addEventListener('click', function(){
+    const caseInput = document.getElementById()
 })
