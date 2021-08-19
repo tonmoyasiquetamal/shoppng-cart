1fbb33c1c534ff4b6e3e4165d813f30d8dc84db9
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-08-19 16:09:20 +0600

    code

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-/* for case plus & case number addition */
-document.getElementById('case-plus').addEventListener('click', function () {
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value ;
-    caseInput.value = parseInt(caseNumber) +1;
+    const caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseInput.value = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+    }
+    
+}
+/* for case plus & case number addition Boolean true */
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber(true);
 });
-/* for case minus & case number subtraction */
-document.getElementById('case-minus').addEventListener('click', function(){
-    const caseInput = document.getElementById()
+/* for case minus & case number subtraction Boolean false */
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
+    // 
 })
